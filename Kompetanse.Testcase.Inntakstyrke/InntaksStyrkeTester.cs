16d023954ff3014443cd52b530f9488b5295812e
Svein-Olav <sveinolav.andersen@fhi.no>
2022-09-30 14:57:33 +0200

    Tester OK.

--- Kompetanse.Testcase.Inntakstyrke/InntaksStyrkeTester.cs
+++ Kompetanse.Testcase.Inntakstyrke/InntaksStyrkeTester.cs
@@ -5,7 +5,7 @@ namespace Kompetanse.Testcase.Inntakstyrke;
  *   - teller , tellerEnhet , nevner , nevnerEnhet    
  *  
  *    f.eks    '1g/2ml'    =>  teller=1 ,tellerEnhet=g , nevner=2 ,  nevnerEnhet=ml
- *    streng kan også inneholde flere verdier, disse skal da splittes med '|' 
+ *    streng kan ogsï¿½ inneholde flere verdier, disse skal da splittes med '|' 
  *    -  '2+1'  =>   teller= 2|1    
  */
 
@@ -38,11 +38,68 @@ public class InntaksStyrkeTester
   
     private (string teller, string tellerEnhet, string nevner, string nevnerEnhet) InntaksStyrkeSplitter(string inntaksStyrke)
     {
+        string teller;
+        string tellerEnhet;
+        string nevner;
+        string nevnerEnhet;
+
         //TODO implementer denne !!!
         //Denne koden kan flyttes i en egen klasse eller hva som helst....
 
-        throw new NotImplementedException("Implementer InntaksStyrkeSplitter() !!!");
-      //  return (null, null, null, null);
+        string pattern = @"([0-9,]*)([a-z]*)";
+
+        var collection = Regex.Matches(inntaksStyrke, pattern, RegexOptions.IgnoreCase);
+
+        if (collection.Count == 4)
+        {
+           
+            teller = collection[0].Groups[1].Value == string.Empty ? null : collection[0].Groups[1].Value;
+            tellerEnhet = collection[0].Groups[2].Value == string.Empty ? null : collection[0].Groups[2].Value;
+            nevner = collection[2].Groups[1].Value == string.Empty ? "1" : collection[2].Groups[1].Value;
+            nevnerEnhet = collection[2].Groups[2].Value == string.Empty ? null : collection[2].Groups[2].Value;
+
+            return (teller, tellerEnhet, nevner, nevnerEnhet);
+        }
+
+        // Test hvor streng innholder %
+        var regexpresion = new Regex("%");
+        if(regexpresion.IsMatch(inntaksStyrke))
+        {
+            pattern = "([0-9,]*)([a-z%]*)";
+
+            collection = Regex.Matches(inntaksStyrke, pattern, RegexOptions.IgnoreCase);
+
+            if (collection.Count == 4)
+            {
+                teller = collection[0].Groups[1].Value + "|" + collection[2].Groups[1].Value;
+                tellerEnhet = collection[0].Groups[2].Value + "|" + collection[2].Groups[2].Value;               
+
+                return (teller, tellerEnhet, null, null);
+            }
+
+        }
+
+        // Test for streng med bare +'er
+        regexpresion = new Regex("\\+");
+        if (regexpresion.IsMatch(inntaksStyrke))
+        {
+            pattern = "([0-9]+)";
+
+            collection = Regex.Matches(inntaksStyrke, pattern, RegexOptions.IgnoreCase);
+
+            teller = String.Empty;
+
+            foreach (Match match in Regex.Matches(inntaksStyrke, pattern, RegexOptions.IgnoreCase))
+              teller = teller + match.Value + "|";
+
+            var utenStrekTilSlutt = teller.Remove(teller.Count()-1);
+            return (utenStrekTilSlutt, null, null, null);
+
+        }
+
+
+
+        return (null, null, null, null);
     }
 
 }
